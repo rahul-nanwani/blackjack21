9aa51544148121f630ed009963177a149b4ea860
Rahul Nanwani <50020924+rahul-nanwani@users.noreply.github.com>
2022-01-10 22:55:57 +0530

    Update players.py

--- src/blackjack21/players.py
+++ src/blackjack21/players.py
@@ -1,22 +1,20 @@
 class PlayerBase:
-    def __init__(self, name, table):
+    def __init__(self, name: str, bet: int, table):
         """
-        Create a base player
+        Create split player for the table
         :param name: str
+        :param bet: int
         :param table: object
         """
         self.name = name
-        self.table = table
+        self.bet = bet
         self.hand = []
-        self.total = 0
         self.bust = False
         self.stand = False
+        self.__table = table
 
-    def _update_total(self):
-        """
-        Update the total for the player
-        :return: None
-        """
+    @property
+    def total(self):
         total = 0
         values = []
         aces = values.count(11)
@@ -28,20 +26,36 @@ class PlayerBase:
                 total -= 10
                 aces -= 1
 
-        self.total = total
+        return total
+
+    @property
+    def result(self):
+        """
+        Result for the player once the player is bust or stands.
+        Negative implies the player loses, positive implies the player wins, and 0 implies a draw.
+        The key below shows the reasons for the result:
+        -2: Player busts
+        -1: Player has less than the dealer
+        0: Player has the same total as the dealer and both are not bust
+        1: Player has 21 (aka. blackjack)
+        2: Player has greater total than the dealer
+        3: The dealer is bust
+        None: player or dealer has not finished playing their hand yet
+        """
+        if (self.bust or self.stand) and (self.__table.dealer.bust or self.__table.dealer.stand):
+            if self.bust:
+                return -2
+            elif self.total < self.__table.dealer.total and not self.__table.dealer.bust:
+                return -1
+            elif self.total == 21:
+                return 1
+            elif self.total > self.__table.dealer.total:
+                return 2
+            elif self.total < self.__table.dealer.total and self.__table.dealer.bust:
+                return 3
+            else:
+                return 0
         return None
-
-
-class Player(PlayerBase):
-    def __init__(self, name: str, bet: int, table: object):
-        """
-        Create split player for the table
-        :param name: str
-        :param bet: int
-        :param table: object
-        """
-        super().__init__(name, table)
-        self.bet = bet
 
     def play_hit(self):
         """
@@ -49,9 +63,8 @@ class Player(PlayerBase):
         :return: Card object
         """
         if not (self.stand or self.bust):
-            card = self.table.deck.pop()
+            card = self.__table.deck.pop()
             self.hand.append(card)
-            self._update_total()
             if self.total > 21:
                 self.bust = True
             elif self.total == 21:
@@ -65,40 +78,11 @@ class Player(PlayerBase):
         """
         if not self.bust:
             self.stand = True
-            self._update_total()
             return True
         return False
 
-    def get_result(self):
-        """
-        Get result for the player once the player is bust or stands.
-        Negative implies the player loses, positive implies the player wins, and 0 implies a draw.
-        The key below shows the reasons for the result:
-        -2: Player busts
-        -1: Player has less than the dealer
-        0: Player has the same total as the dealer and both are not bust
-        1: Player has 21 (aka. blackjack)
-        2: Player has greater total than the dealer
-        3: The dealer is bust
-        :return: int, None if player or dealer has not finished playing their hand yet
-        """
-        if (self.bust or self.stand) and (self.table.dealer.bust or self.table.dealer.stand):
-            if self.bust:
-                return -2
-            elif self.total < self.table.dealer.total and not self.table.dealer.bust:
-                return -1
-            elif self.total == 21:
-                return 1
-            elif self.total > self.table.dealer.total:
-                return 2
-            elif self.total < self.table.dealer.total and self.table.dealer.bust:
-                return 3
-            else:
-                return 0
-        return None
 
-
-class PlayerSuper(Player):
+class Player(PlayerBase):
     def __init__(self, name: str, bet: int, table):
         """
         Create player for the table
@@ -149,30 +133,50 @@ class PlayerSuper(Player):
         :return: Player object
         """
         if self.can_split():
-            self.split = Player(self.name, self.bet, self.table)
+            self.split = PlayerBase(self.name, self.bet, self.__table)
             self.split.hand.append(self.hand.pop())
-            self._update_total()
-            self.split._update_total()
             return self.split
 
 
-class Dealer(PlayerBase):
+class Dealer:
     def __init__(self, name, table):
         """
         Create a dealer for the table
         :param name: str
         """
-        super().__init__(name, table)
+        self.name = name
+        self.hand = []
+        self.bust = False
+        self.stand = False
+        self.__table = table
+        self.__play_hand = False
 
-    def _play_hit(self):
+    @property
+    def total(self):
+        if not self.__play_hand:
+            return self.hand[0]._value
+        else:
+            total = 0
+            values = []
+            aces = values.count(11)
+            for card in self.hand:
+                values.append(card._value)
+            for value in values:
+                total += value
+                while aces > 0 and total > 21:
+                    total -= 10
+                    aces -= 1
+
+            return total
+
+    def __play_hit(self):
         """
         Deals another card to the dealer if the dealer is not busted or has played stand
         :return: Card object
         """
         if not (self.stand or self.bust):
-            card = self.table.deck.pop()
+            card = self.__table.deck.pop()
             self.hand.append(card)
-            self._update_total()
             if self.total > 21:
                 self.bust = True
             elif self.total == 21:
@@ -186,15 +190,15 @@ class Dealer(PlayerBase):
         Second card will be revealed only after the result.
         :return: True if dealt successfully, else False
         """
-        if len(self.table.players) > 0:
-            self._play_hit()
+        if len(self.__table.players) > 0:
+            self.__play_hit()
 
-            for player in self.table.players:
+            for player in self.__table.players:
                 player.play_hit()
-            for player in self.table.players:
+            for player in self.__table.players:
                 player.play_hit()
 
-            self.hand.append(self.table.deck.pop())
+            self.hand.append(self.__table.deck.pop())
 
             return True
         else:
@@ -205,14 +209,14 @@ class Dealer(PlayerBase):
         Play the dealer once everyone has played their hands
         :return: None
         """
-        self._update_total()
+        self.__play_hand = True
         totals = []
-        for player in self.table.players:
+        for player in self.__table.players:
             totals.append(player.total)
         highest_total = max(totals)
 
         while self.total < 17 and self.total < highest_total:
-            self._play_hit()
+            self.__play_hit()
         if self.total > 21:
             self.bust = True
         else:
