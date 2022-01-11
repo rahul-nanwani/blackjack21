e040f8a169708ba7d2d7dd2170cf86401685bf42
Rahul Nanwani <50020924+rahul-nanwani@users.noreply.github.com>
2022-01-11 14:42:22 +0530

    Update __init__.py

--- src/blackjack21/__init__.py
+++ src/blackjack21/__init__.py
@@ -5,6 +5,6 @@ Documentation: https://github.com/rahul-nanwani/blackjack21/wiki
 from .table import Table
 
 __title__ = "blackjack21"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Rahul Nanwani"
 __license__ = "MIT License"
