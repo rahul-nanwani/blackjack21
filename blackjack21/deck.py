dbf4058c33dcb903d711c8751fb56ef00d2572c7
Rahul Nanwani <50020924+rahul-nanwani@users.noreply.github.com>
2023-08-27 14:26:06 +0530

    Improved code readability

--- blackjack21/deck.py
+++ blackjack21/deck.py
@@ -1,35 +1,12 @@
-# MIT License
-#
-# Copyright (c) 2022 Rahul Nanwani
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+__all__ = ('Card', 'Deck')
 
-from itertools import chain
 from random import shuffle
-from typing import List
+from typing import List, Text, Tuple, Any, Generator
 
 from .exceptions import InvalidSuits, InvalidRanks
 
-__all__ = (
-    "Card",
-    "Deck",
-)
+Suits = Tuple[Text, Text, Text, Text]
+Ranks = Tuple[Text, Text, Text, Text, Text, Text, Text, Text, Text, Text, Text, Text, Text]
 
 class Card:
     """Card class
@@ -40,30 +17,29 @@ class Card:
     """
 
     __slots__ = (
-        "_suit",
-        "_rank",
-        "_value",
+        '_suit',
+        '_rank',
+        '_value',
     )
 
-    def __init__(self, suit: str, rank: str, value: int):
+    def __init__(self, suit: Text, rank: Text, value: int):
         self._suit = suit
         self._rank = rank
         self._value = value
 
-    # dunder methods
-    def __repr__(self):
+    def __repr__(self) -> Text:
         return f"{self.rank} of {self.suit}"
 
-    def __str__(self):
+    def __str__(self) -> Text:
         return f"{self.rank} of {self.suit}"
 
-    # properties
     @property
-    def suit(self) -> str:
+    def suit(self) -> Text:
         """Card suit"""
         return self._suit
+
     @property
-    def rank(self) -> str:
+    def rank(self) -> Text:
         """Card rank"""
         return self._rank
 
@@ -74,80 +50,83 @@ class Card:
 
 
 class Deck:
+    # noinspection PyUnresolvedReferences
     """Deck of cards class (Iterable)
-
-    :param suits: tuple of 4 suits
-    :param ranks: tuple of 13 card ranks
-
-    :keyword count: int number of decks to be merged
-
-    :raises InvalidSuits: if length of suits is not 4
-    :raises InvalidRanks: if length of ranks is not 13
-    """
+    
+        :param suits: tuple of 4 suits
+        :param ranks: tuple of 13 card ranks
+    
+        :keyword count: int number of decks to be merged
+    
+        :raises InvalidSuits: if length of suits is not 4
+        :raises InvalidRanks: if length of ranks is not 13
+        """
 
     __slots__ = (
-        "_cards",
-        "_drawn_cards",
+        '_cards',
+        '_drawn_cards',
     )
 
-    def __init__(self, suits: tuple, ranks: tuple, **kwargs):
+    def __init__(self, suits: Suits, ranks: Ranks, **kwargs: Any):
+
         if len(suits) != 4:
             raise InvalidSuits(suits)
+
         if len(ranks) != 13:
             raise InvalidRanks(ranks)
+        
+        self._cards = []
 
-        self._cards = map(
-            lambda suit: map(
-                lambda rank, value: Card(suit, rank, value),
-                ranks, [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-            ),
-            suits
-        )
-        self._cards = list(chain(*self._cards))
+        for suit in suits:
+            for rank, value in zip(ranks, (11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)):
+                self._cards.append(Card(suit, rank, value))
+            
         self._cards *= kwargs.get('count', 1)
         self._drawn_cards = []
+        
         shuffle(self._cards)
 
-    # dunder methods
-    def __repr__(self):
+    def __repr__(self) -> Text:
         return f"<Deck cards: {len(self._cards)}>"
 
-    def __str__(self):
+    def __str__(self) -> Text:
         return f"<Deck cards: {len(self._cards)}>"
 
-    def __iter__(self):
+    def __iter__(self) -> Generator['Card', None, None]:
         """Iterate through the cards in the deck"""
         yield from self._cards
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> 'Card':
         """Get card at index"""
         return self._cards[index]
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Number of cards left in the deck"""
         return len(self._cards)
 
-    # properties
     @property
-    def cards(self) -> List[Card]:
+    def cards(self) -> List['Card']:
         """List of Card class objects currently in the deck"""
         return self._cards
 
     @property
-    def drawn_cards(self) -> List[Card]:
+    def drawn_cards(self) -> List['Card']:
         """List of Card class objects drawn from the deck"""
         return self._drawn_cards
 
-    # methods
-    def shuffle(self):
+    def shuffle(self) -> None:
         """Shuffle the cards in the deck"""
-        shuffle(self._cards)
 
-    def draw_card(self) -> Card:
+        shuffle(self._cards)
+        return None
+
+    def draw_card(self) -> 'Card':
         """Draw a card from the deck
 
         :return: Card object
         """
+        
         drawn_card = self._cards.pop()
         self._drawn_cards.append(drawn_card)
+        
         return drawn_card
--- blackjack21/players.py
+++ blackjack21/players.py
@@ -1,38 +1,20 @@
-# MIT License
-#
-# Copyright (c) 2022 Rahul Nanwani
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+__all__ = (
+    'PlayerBase',
+    'Player',
+    'Hand',
+    'Result',
+)
 
-from typing import Union, List
+from typing import List, Text, Generator, Optional, TYPE_CHECKING
 
 from .deck import Card
 from .exceptions import PlayFailure
 
-__all__ = (
-    "PlayerBase",
-    "Player",
-    "Hand",
-    "Result",
-)
+if TYPE_CHECKING:
+    from . import Table
+
 Hand = List[Card]
-Result = Union[int, None]
+Result = Optional[int]
 
 
 class PlayerBase:
@@ -44,15 +26,15 @@ class PlayerBase:
     """
 
     __slots__ = (
-        "_name",
-        "_bet",
-        "_hand",
-        "_bust",
-        "_stand",
-        "_table",
+        '_name',
+        '_bet',
+        '_hand',
+        '_bust',
+        '_stand',
+        '_table',
     )
 
-    def __init__(self, name: str, bet: int, table):
+    def __init__(self, name: Text, bet: int, table: 'Table'):
         self._name = name
         self._bet = bet
         self._hand = []
@@ -60,28 +42,26 @@ class PlayerBase:
         self._stand = False
         self._table = table
 
-    # dunder methods
-    def __repr__(self):
+    def __repr__(self) -> Text:
         return self._name
 
-    def __str__(self):
+    def __str__(self) -> Text:
         return self._name
 
-    def __iter__(self):
+    def __iter__(self) -> Generator['Card', None, None]:
         """Iterate through the cards in hand"""
         yield from self._hand
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> 'Card':
         """Get card at index"""
         return self._hand[index]
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Number of cards in the hand"""
         return len(self._hand)
 
-    # properties
     @property
-    def name(self) -> str:
+    def name(self) -> Text:
         """Player name"""
         return self._name
 
@@ -111,7 +91,11 @@ class PlayerBase:
         values = [card.value for card in self.hand]
         aces = values.count(11)
         total = sum(values)
-        while aces > 0 and total > 21: total -= 10; aces -= 1
+        
+        while aces > 0 and total > 21:
+            total -= 10
+            aces -= 1
+        
         return total
 
     @property
@@ -120,40 +104,53 @@ class PlayerBase:
 
         Negative implies the player loses, positive implies the player wins, and 0 implies a draw.
         The key below shows the reasons for the result:
-            :key -2: Player busts
-            :key -1: Player has less than the dealer
-            :key 0: Player has the same total as the dealer and both are not bust
-            :key 1: Player has 21 (aka. blackjack)
-            :key 2: Player has greater than the dealer
-            :key 3: The dealer is bust
-            :key None: Dealer has not finished playing yet
+            :key -2: Player busts.
+            :key -1: Player has less than the dealer.
+            :key 0: Player has the same total as the dealer and both are not bust.
+            :key 1: Player has 21 (aka. blackjack).
+            :key 2: Player has greater than the dealer.
+            :key 3: The dealer is bust.
+            :key None: Dealer has not finished playing yet.
         """
-        if not any((self._table.dealer.bust, self._table.dealer.stand,)):
+        
+        if not (self._table.dealer.bust or self._table.dealer.stand):
             return None
+
         elif self.bust:
             return -2  # Player is bust
+
         elif self.total < self._table.dealer.total and not self._table.dealer.bust:
             return -1  # Player has less than the dealer
+
         elif self.total == 21:
             return 1  # Player has 21 (aka. blackjack)
+
         elif self.total > self._table.dealer.total:
             return 2  # Player has greater than the dealer
+
         elif self.total < self._table.dealer.total and self._table.dealer.bust:
             return 3  # The dealer is bust
+
         else:
             return 0  # Player has the same total as the dealer and both are not bust
 
     # methods
-    def play_hit(self) -> Card:
+    def play_hit(self) -> 'Card':
         """Deals another card to the player if the player is not busted or has played stand
 
         :return: Card class object
         """
+        
         if not (self.stand or self.bust):
             card = self._table.deck.draw_card()
             self._hand.append(card)
-            if self.total > 21: self._bust = True
-            if self.total == 21: self._stand = True
+        
+            if self.total > 21:
+                self._bust = True
+                
+            if self.total == 21:
+                self._stand = True
+
             return card
 
     def play_stand(self) -> bool:
@@ -161,10 +158,13 @@ class PlayerBase:
 
         :return: bool
         """
+        
         if not self.bust:
             self._stand = True
             return True
-        return False
+        
+        else:
+            return False
 
 
 class Player(PlayerBase):
@@ -176,52 +176,58 @@ class Player(PlayerBase):
     """
 
     __slots__ = (
-        "_split",
+        '_split',
     )
 
-    def __init__(self, name: str, bet: int, table):
+    def __init__(self, name: Text, bet: int, table: 'Table'):
         super().__init__(name, bet, table)
         self._split = None
 
-    # properties
     @property
     def bet(self) -> int:
         return self._bet
 
     @property
-    def split(self) -> Union[PlayerBase, None]:
+    def split(self) -> Optional['PlayerBase']:
         """PlayerBase class object if split is played else none"""
         return self._split
 
     @property
     def can_double_down(self) -> bool:
         """bool if the player is eligible to play double down"""
-        return True if len(self.hand) == 2 and not self.split else False
+        
+        if len(self.hand) == 2 and not self.split:
+            return True
+        
+        else:
+            return False
 
     @property
     def can_split(self) -> bool:
         """bool if the player is eligible to play split"""
-        if len(self.hand) == 2:
-            ranks = [card.rank for card in self.hand]
-            return ranks[0] == ranks[1]
 
-    # methods
-    def play_double_down(self) -> Card:
+        if len(self.hand) == 2:
+            return self.hand[0].rank == self.hand[1].rank
+
+    def play_double_down(self) -> 'Card':
         """Double down can be played only on the first turn by doubling the bet amount and will hit only once
 
         :return: Card class object
 
         :raises PlayFailure: if player is not eligible to play this
         """
+        
         if self.can_double_down:
             self._bet *= 2
             card = self.play_hit()
             self.play_stand()
+        
             return card
+
         else:
             raise PlayFailure(self.name, "double down")
 
-    def play_split(self) -> PlayerBase:
+    def play_split(self) -> 'PlayerBase':
         """Split can be played only on the first turn by splitting the hand if both the cards have the same ranks.
         The bet will remain the same on both the hands.
         If the player bets 100 initially, so after playing split the player will have placed 100 on first hand,
@@ -231,9 +237,12 @@ class Player(PlayerBase):
 
         :raises PlayFailure: if player is not eligible to play this
         """
+        
         if self.can_split:
             self._split = PlayerBase(self.name, self.bet, self._table)
             self._split.play_hit()
+            
             return self._split
+        
         else:
             raise PlayFailure(self.name, "split")
